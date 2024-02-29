18a22d741f5e781f5ad8f32bef7a467e40d137e2
Rommel05 <rrll20220@gmail.com>
2024-02-29 10:07:07 +0100

    Ejemplo BD

--- src/main/java/Network.java
+++ src/main/java/Network.java
@@ -1,2 +1,8 @@
+import java.sql.SQLException;
 public class Network {
+    private static  java.sql.Connection con;
+    public static void main(String[] args) throws SQLException {
+        String host = "jdbc:sqlite:src/main/resources/network";
+        con = java.sql.DriverManager.getConnection( host );
+    }
 }
